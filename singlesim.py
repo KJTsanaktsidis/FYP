6ae4d23343a5045ddba05fccc8732f32fa1ef1ca
KJ Tsanaktsidis <kjtsanaktsidis@gmail.com>
2013-05-26 16:08:10 +1000

    Set up singlesim.py to go multi T

--- singlesim.py
+++ singlesim.py
@@ -17,25 +17,14 @@ ds = InputDatastore('../InputData', 'NiCu')
 
 #--CHANGEME
 #simdata_I = cse.compute(z, cvf, current, direction)
-ndt = int(10 // defaults.simulation_dt)
-cse = CalcSimExecutor(ds, T, ndt=ndt)
-simdata_10s = cse.compute(z, cvf, 0, 'forward')
-
-ndt = int(60 // defaults.simulation_dt)
-cse = CalcSimExecutor(ds, T, ndt=ndt)
-simdata_1m = cse.compute(z, cvf, 0, 'forward')
-
 ndt = int(600 // defaults.simulation_dt)
-cse = CalcSimExecutor(ds, T, ndt=ndt)
-simdata_10m = cse.compute(z, cvf, 0, 'forward')
+Tks = [1049, 1049.5, 1050, 1050.5, 1051, 1051.5, 1052]
+simds = []
 
-ndt = int(30 * 60 // defaults.simulation_dt)
-cse = CalcSimExecutor(ds, T, ndt=ndt)
-simdata_30m = cse.compute(z, cvf, 0, 'forward')
-
-ndt = int(60 * 60 // defaults.simulation_dt)
-cse = CalcSimExecutor(ds, T, ndt=ndt)
-simdata_1h = cse.compute(z, cvf, 0, 'forward')
+for Tk in Tks:
+    cse = CalcSimExecutor(ds, Tk, ndt=ndt)
+    sd = cse.compute(z, cvf, 0, 'forward')
+    simds.append(sd)
 
 #shiftengine = ComparisonEngine(cse.cs)
 #shiftengine.calibrate(simdata_I[:, 1], simdata_noI[:, 1])
@@ -43,12 +32,9 @@ simdata_1h = cse.compute(z, cvf, 0, 'forward')
 
 figure()
 
-#--CHANGEME
-plot(simdata_10s[:, 0], simdata_10s[:, 1], label='t = 10s')
-plot(simdata_1m[:, 0], simdata_1m[:, 1], label='t = 1m')
-plot(simdata_10m[:, 0], simdata_10m[:, 1], label='t = 10m')
-plot(simdata_30m[:, 0], simdata_30m[:, 1], label='t = 30m')
-plot(simdata_1h[:, 0], simdata_1h[:, 1], label='t = 1h')
+ndxmult = 1
+for Tk, simd in zip(Tks, simds):
+    plot(simd[(105*ndxmult):(120*ndxmult), 0], simd[(105*ndxmult):(120*ndxmult), 1], label='T = {}K'.format(Tk))
 
 xlabel('Position (micron)')
 ylabel('Cu Composition (at. fraction)')
